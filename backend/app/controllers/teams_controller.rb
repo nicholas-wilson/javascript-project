3adb6aafdbb7ee80421fb77adacfebb184ad1498
nicholas-wilson <nicholas.wilson@nevada.unr.edu>
2020-07-20 16:04:55 -0700

    added more controller actions for TeamsController

--- backend/app/controllers/teams_controller.rb
+++ backend/app/controllers/teams_controller.rb
@@ -1,6 +1,15 @@
 class TeamsController < ApplicationController
+  def create
+    team = Team.create
+    render json: team
+  end
+
   def show
     team = Team.find_by(:id => params[:id])
     render json: team, include: [:units]
+  end
+
+  def update
+    # use this when I recruit new unit or earn more money
   end
 end
