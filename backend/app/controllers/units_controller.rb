b7dd3b29f00b9210cb5511bc3b5f2cda60712a32
nicholas-wilson <nicholas.wilson@nevada.unr.edu>
2020-07-27 15:42:51 -0700

    removed extra routes

--- backend/app/controllers/units_controller.rb
+++ backend/app/controllers/units_controller.rb
@@ -10,11 +10,6 @@ class UnitsController < ApplicationController
     render json: dude
   end
 
-  def update
-    dude = Unit.find_by(id: params["id"])
-    dude.update(name: params["name"], atk: params["atk"], def: params["def"], speed: params["speed"], max_hp: params["max_hp"], current_hp: params["current_hp"]);
-  end
-
   def destroy
     Unit.find_by(id: params["id"]).delete
   end
