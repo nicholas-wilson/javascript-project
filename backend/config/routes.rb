582fd552fa12ac8d39936775bd435562e272b118
nicholas-wilson <nicholas.wilson@nevada.unr.edu>
2020-07-27 16:01:21 -0700

    properly renamed the resources in the routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :teams, only: [:create, :update, :show]
-  resources :units, only: [:create, :delete]
+  resources :units, only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
